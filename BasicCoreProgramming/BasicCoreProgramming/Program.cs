822df49831f98da223c52f53894add73c98fdb89
akshayb407 <akshayb407@gmail.com>
2022-07-28 20:00:47 +0530

    .gitignore

---
+++ BasicCoreProgramming/BasicCoreProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCoreProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programming!");
+        }
+    }
+}
